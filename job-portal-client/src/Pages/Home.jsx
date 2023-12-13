d7dfa4c3d6e6fa71610ac9155627e86b1b1cfa4f
amalnathvp <amalnathvp31@gmail.com>
2023-12-13 22:25:51 +0530

    banner

--- job-portal-client/src/Pages/Home.jsx
+++ job-portal-client/src/Pages/Home.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+
+import React, { useState} from 'react'
 import Banner from '../components/Banner'
 
 const Home = () => {
-  // const [query, setQuery] = useState("");
-  //   const handleInputChange = (event) => {
-  //       setQuery(event.target.value)
-  //       console.log(query);
-  //   }
+  const [query, setQuery] = useState("");
+    const handleInputChange = (event) => {
+        setQuery(event.target.value)
+        console.log(query);
+    }
   return (
-    <Banner/>
+    <Banner query={query} handleInputChange={handleInputChange}/>
   )
 }
 
