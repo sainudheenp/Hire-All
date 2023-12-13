0990b43462e7ad95e33bccc41dbf6779e7a2cb82
amalnathvp <amalnathvp31@gmail.com>
2023-12-13 22:09:33 +0530

    bug

--- job-portal-client/src/Pages/Home.jsx
+++ job-portal-client/src/Pages/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import Banner from '../components/Banner'
 
 const Home = () => {
+  // const [query, setQuery] = useState("");
+  //   const handleInputChange = (event) => {
+  //       setQuery(event.target.value)
+  //       console.log(query);
+  //   }
   return (
     <Banner/>
   )
--- job-portal-client/src/components/Banner.jsx
+++ job-portal-client/src/components/Banner.jsx
@@ -7,6 +7,7 @@ const Banner = () => {
     const handleInputChange = (event) => {
         setQuery(event.target.value)
     }
+    console.log(query);
   return (
     <div className='max-w-screen-2xl container mx-auto xl:px-24 px-4 md:py-20 py-14'>
         <h1 className='text-5xl font-bold text-primary mb-3'>Find your <span className='text-blue'>new job</span> Today</h1>
