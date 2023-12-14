853b9dc7472ad2d3ef136f1a0055fa8711ccc56e
amalnathvp <amalnathvp31@gmail.com>
2023-12-14 14:14:52 +0530

    main page design started

--- job-portal-client/src/components/Banner.jsx
+++ job-portal-client/src/components/Banner.jsx
@@ -28,7 +28,7 @@ const Banner = ({query, handleInputChange}) => {
                 <div className='flex md:rounded-s-none rounded shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within-ring-indigo-600 md:w-1/3 w-full'>
                     <input type="text" name='title' id='title' placeholder='Location'
                     className='block flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900 placeholder: text-gray-400 focus:right-0 sm:text-sm sm:leading-6'
-                    value={""}
+                    // value={""}
                      />
                      <FiMapPin className='absolute mt-2.5 ml-2 text-gray-400' />
 
